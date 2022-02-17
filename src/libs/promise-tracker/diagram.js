c58f602085e0329b5609d177866a076de1d9e006
Chris Mceniry <cmceniry@mit.edu>
2022-02-16 21:20:00 -0800

    First pass on diagram code generation

---
+++ src/libs/promise-tracker/diagram.js
@@ -0,0 +1,46 @@
+function d({component, behavior, satisfied, unsatisfied}) {
+  let ret = []
+  if (satisfied) {
+    ret.push("rect rgb(0,255,0)");
+    satisfied.forEach((e) => {
+      ret.push(`    ${component} ->> ${e.component}: ${behavior}`);
+      if (e.conditions) {
+        e.conditions.forEach((c) => {
+          const child = d({
+            behavior: c.behavior,
+            component: e.component,
+            satisfied: c.satisfied,
+          });
+          ret = [...ret, ...(child.map((l) => "    " + l))];  
+        });
+      };
+    });
+    ret.push("end");
+  }
+  if (unsatisfied) {
+    ret.push("rect rgb(255,0,0)");
+    if (unsatisfied.length > 0) {
+      unsatisfied.forEach((e) => {
+        ret.push(`    ${component} ->> ${e.component}: ${behavior}`);
+        if (e.conditions) {
+          e.conditions.forEach((c) => {
+            const child = d({
+              behavior: c.behavior,
+              component: e.component,
+              unsatisfied: c.unsatisfied,
+            });
+            ret = [...ret, ...(child.map((l) => "    " + l))];
+          });
+        };
+      });
+    } else {
+      ret.push(`    ${component} -X ${component}: ${behavior}`);
+    }
+    ret.push("end");
+  }
+  return ret;
+};
+
+export default function diagram(input) {
+  return ["sequenceDiagram", ...(d(input).map((l) => "    " + l))].join("\n");
+};
