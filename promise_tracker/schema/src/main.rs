b6a3afad3ffa230955bc0f463f33e472a72806ef
Chris Mceniry <cmceniry@mit.edu>
2023-09-16 20:07:44 -0700

    Generalize schema command to more than jsut agent

--- promise_tracker/schema/src/main.rs
+++ promise_tracker/schema/src/main.rs
@@ -1,12 +1,19 @@
-use schemars::schema_for;
+use schemars::{schema_for,JsonSchema};
+use serde;
 use serde_json::{self};
 
 use promise_tracker::components::SuperAgent;
 use promise_tracker::components::Agent;
 
+#[derive(JsonSchema)]
+#[serde(tag = "kind")]
+#[serde(deny_unknown_fields)]
+enum PTComponent {
+    Agent(Agent),
+    SuperAgent(SuperAgent),
+}
+
 fn main() {
-    let schema = schema_for!(SuperAgent);
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
-    let schema = schema_for!(Agent);
+    let schema = schema_for!(PTComponent);
     println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 }
